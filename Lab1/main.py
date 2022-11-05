16f07486316df425a3c2717b2e9b22236fdea015
Dmitrii14 <117352471+Dmitrii14@users.noreply.github.com>
2022-11-05 16:03:15 +0300

    Сохранение картинки в папку

--- Lab1/main.py
+++ Lab1/main.py
@@ -2,3 +2,9 @@ import os, shutil, requests
 from bs4 import BeautifulSoup as BS
 
 URL = "https://yandex.ru/images/" # ссылка на страничку html
+def save_image(image_url, name, i):
+    """сохранение картинки в папку"""
+    req = requests.get(f"https:{image_url}")
+    saver = open(f"dataset/{name}/{i:04d}.jpg", "wb")
+    saver.write(req.content)
+    saver.close()
