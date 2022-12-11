53f00337e4b9f3bd4ddb39dab64c79fc60b08763
Dmitrii14 <117352471+Dmitrii14@users.noreply.github.com>
2022-12-11 19:04:13 +0300

    Функция копирования и рандомного переименовывания

--- Lab2/copy_elements.py
+++ Lab2/copy_elements.py
@@ -2,7 +2,6 @@ import os
 import shutil
 import Main
 from Main import Data
-from typing import Type
 
 
 def make_dir(obj: type(Data)) -> None:
---
+++ Lab2/random_copy.py
@@ -0,0 +1,25 @@
+import os
+import random
+import shutil
+import copy_elements as ce
+import Main
+from Main import Data
+
+
+def create_copy_dataset_with_random_number(obj: type(Data), path: str, class_name: str) -> None:
+    later_dir = obj.dir_name
+    ce.make_dir(obj)
+
+    for i in range(1000):
+        n = random.randint(0, 10000)
+        os.rename(os.path.join(later_dir, class_name, f'{(i+1):04d}.jpg'),
+                  os.path.join(later_dir, class_name, f'{n:05d}.jpg'))
+        shutil.copy(os.path.join(later_dir, class_name, f'{n:05d}.jpg'), obj.dir_name)
+        os.rename(os.path.join(later_dir, class_name, f"{n:05d}.jpg"),
+                  os.path.join(later_dir, class_name, f'{(i+1):04d}.jpg'))
+        obj.add(os.path.join(path, obj.dir_name, class_name), class_name, f'{n:05d}.jpg')
+
+
+if __name__ == "__main__":
+    create_copy_dataset_with_random_number(Data("dataset"), "C:\Users\User\Documents\GitHub\python_var4\Lab2",
+                                           Main.CLASS_DEFAULT[0])
