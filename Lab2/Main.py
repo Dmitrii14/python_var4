10f4a434142988492d08c53e7488ec3305bb320f
Dmitrii14 <117352471+Dmitrii14@users.noreply.github.com>
2022-12-11 13:45:20 +0300

    Функция добавления строки в файл

--- Lab2/Main.py
+++ Lab2/Main.py
@@ -9,3 +9,17 @@ class Data:
         self.number_lines = 0
         self.viewed_files = 1
         self.dir_name = dir_name
+
+    def add(self, path: str, class_name: str, name_image: str) -> None:
+        with open("annotation.csv", "a", encoding="utf-8", newline="") as file:
+            writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+            if self.number_lines == 0:  # если кол-во строк = 0, то это заголовки файла аннотация
+                writer.writerow([
+                    "абсолютный путь",
+                    "относительный путь",
+                    "класс"
+                ])
+                self.number_lines += 1
+            writer.writerow([os.path.join(path, self.dir_name, class_name, name_image),
+                             os.path.join(self.dir_name, class_name, name_image), class_name])
+            self.number_lines += 1
