4eaec9313231348d521bd8e0aad38ccc4017af37
Dmitrii14 <117352471+Dmitrii14@users.noreply.github.com>
2022-12-11 18:41:59 +0300

    Функция создания новой папки и копирования в неё каталога и подкаталогов

--- Lab2/copy_elements.py
+++ Lab2/copy_elements.py
@@ -13,3 +13,19 @@ def make_dir(obj: type(Data)) -> None:
         shutil.rmtree("new_dataset")
         os.mkdir("new_dataset")
         obj.dir_name = "new_dataset"
+
+
+def teleport_dir(obj: type(Data), path: str, class_name: str) -> None:
+    later_dir = obj.dir_name
+    make_dir(obj)
+    for i in range(1000):
+        os.rename(os.path.join(later_dir, class_name, f'{(i+1):04d}.jpg'),
+                  os.path.join(later_dir, class_name, f'{class_name}_{(i+1):04d}.jpg'))
+        shutil.copy(os.path.join(later_dir, class_name, f'{class_name}_{(i+1):04d}.jpg'), obj.dir_name)
+        os.rename(os.path.join(later_dir, class_name, f"{class_name}_{(i+1):04d}.jpg"),
+                  os.path.join(later_dir, class_name, f'{(i+1):04d}.jpg'))
+        obj.add(os.path.join(path, obj.dir_name, class_name), class_name, f'{class_name}_{(i+1):04d}.jpg')
+
+
+if __name__ == "__main__":
+    teleport_dir(Data("dataset"), "C:\Users\User\Documents\GitHub\python_var4\Lab2", Main.CLASS_DEFAULT[0])
