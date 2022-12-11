a36e791861c5fe5be5a315c7b65fbd504def301f
Dmitrii14 <117352471+Dmitrii14@users.noreply.github.com>
2022-12-11 15:46:15 +0300

    Дописал класс

--- Lab2/iterator_class.py
+++ Lab2/iterator_class.py
@@ -1,15 +1,18 @@
-import Main
-from Main import Data
 import next_element as ne
-import os
+from Main import Data
 
 
 class IteratorOfExemplar:
-    """
-        класс итератор - чтобы обойти элементы внутри объекта вашего собственного класса
-    """
-
     def __init__(self, obj: type(Data), pointer: str):
         self.obj = obj
         self.pointer = pointer
         self.counter = 0
+
+    def __next__(self) -> str:
+        self.pointer = ne.next_element(self.obj, self.pointer)
+        return self.pointer
+
+
+if __name__ == "__main__":
+    iter = IteratorOfExemplar(Data("dataset"),  "dataset\\rose\\0002.jpg")
+    print(iter.__next__())
