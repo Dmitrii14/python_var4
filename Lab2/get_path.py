fbbfce0ac8c8767eb17bea688ace31b1580e10e4
Dmitrii14 <117352471+Dmitrii14@users.noreply.github.com>
2022-12-22 16:03:24 +0300

    Создание файла для получения пути к файлам

---
+++ Lab2/get_path.py
@@ -0,0 +1,22 @@
+import os
+
+
+def download_relative_way(name_class, number):
+    return f"dataset/{name_class}/{str(number).zfill(4)}.jpg"
+
+
+def changed_relative_way(name_class, number):
+    return f"dataset/copy_elements/{name_class}_{str(number).zfill(4)}.jpg"
+
+
+def random_relative_way(number):
+    return f"dataset/random_copy/{str(number).zfill(4)}.jpg"
+
+
+def get_absolute_way(name_class, number, mode):
+    if mode == "download":
+        return os.path.abspath(download_relative_way(name_class, number))
+    if mode == "changed":
+        return os.path.abspath(changed_relative_way(name_class, number))
+    if mode == "random":
+        return os.path.abspath(random_relative_way(number))
