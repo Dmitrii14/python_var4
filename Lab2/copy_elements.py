6e0746e3e9911af619e3ea354e169d54319558f4
Dmitrii14 <117352471+Dmitrii14@users.noreply.github.com>
2022-12-22 16:06:00 +0300

    Переписал файл копирования элементов

--- Lab2/copy_elements.py
+++ Lab2/copy_elements.py
@@ -1,30 +1,32 @@
+import csv
 import os
+import get_path
 import shutil
-import main
-from main import Data
 
 
-def make_dir(obj: type(Data)) -> None:
-    try:
-        os.mkdir("new_dataset")
-        obj.dir_name = "new_dataset"
-    except OSError:
-        shutil.rmtree("new_dataset")
-        os.mkdir("new_dataset")
-        obj.dir_name = "new_dataset"
+def copy_to_another(class_name):
+    with open("changed_annotation.csv", mode="a", encoding='utf-8') as w_file:
+        file_writer = csv.writer(w_file, delimiter=";", lineterminator="\r")
+        file_writer.writerow(["Абсолютный путь", "Относительный путь", "Класс"])
+        for i in range(1050):
+            if (os.path.isfile(get_path.get_absolute_way(class_name, i, "download")) == True):
+                shutil.copyfile(get_path.get_absolute_way(class_name, i, "download"),
+                                get_path.get_absolute_way(class_name, i, "changed"))
+                file_writer.writerow(
+                    [get_path.get_absolute_way(class_name, i, "download"), get_path.changed_relative_way(class_name, i),
+                     class_name])
 
 
-def teleport_dir(obj: type(Data), path: str, class_name: str) -> None:
-    later_dir = obj.dir_name
-    make_dir(obj)
-    for i in range(1000):
-        os.rename(os.path.join(later_dir, class_name, f'{(i+1):04d}.jpg'),
-                  os.path.join(later_dir, class_name, f'{class_name}_{(i+1):04d}.jpg'))
-        shutil.copy(os.path.join(later_dir, class_name, f'{class_name}_{(i+1):04d}.jpg'), obj.dir_name)
-        os.rename(os.path.join(later_dir, class_name, f"{class_name}_{(i+1):04d}.jpg"),
-                  os.path.join(later_dir, class_name, f'{(i+1):04d}.jpg'))
-        obj.add(os.path.join(path, obj.dir_name, class_name), class_name, f'{class_name}_{(i+1):04d}.jpg')
+def main():
+    print("Start changing")
+    if not os.path.isdir("dataset/changed_dataset"):
+        os.mkdir("dataset/changed_dataset")
+    class_name = "rose"
+    copy_to_another(class_name)
+    class_name = "tulip"
+    copy_to_another(class_name)
+    print("The end of changing")
 
 
 if __name__ == "__main__":
-    teleport_dir(Data("dataset"), "C:\Users\User\Documents\GitHub\python_var4\Lab2", main.CLASS_DEFAULT[0])
+    main()
