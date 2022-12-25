ff67617b13e6ea87bb401025254129a99152f799
Dmitrii14 <117352471+Dmitrii14@users.noreply.github.com>
2022-12-25 19:10:43 +0300

    Методы изменения стобцов ширины высоты глубины изображения и вычисления информации для столбцов

--- main.py
+++ main.py
@@ -37,4 +37,20 @@ class DataAnalysis:
         self.df['height'] = height_image
         self.df['channel'] = channels_image
 
+    def change_columns_dataframe(self) -> pd.DataFrame:
+        """
+            данная функция предназначена для изменения столбцов dataframe, добавления
+            столбцов высоты, ширины, глубины изображения и вычисления статистической информации для столбцов.
+        """
+        self.image_forms(self.df["related_path"])
+        self.saving_in_csv_file("property.csv")
 
+    def forming_data_frame(self, filename: str = 'annotation.csv'):
+        """
+            данная функция с помощью pandas формирует DataFrame, происходит изменение названия колонок
+            :df: - это dataframe
+        """
+        self.df = pd.read_csv(filename)
+        self.df.drop(['абсолютный путь'], axis=1, inplace=True)
+        self.df = self.df.rename(columns={'относительный путь': 'related_path', 'класс': 'name_class'})
+        self.change_columns_dataframe()
