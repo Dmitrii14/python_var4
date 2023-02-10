7877c9776f3f07a3f03ffb496af4810178a55558
Dmitrii14 <117352471+Dmitrii14@users.noreply.github.com>
2023-02-10 19:42:24 +0300

    Создал класс для загрузки изображений

--- Lab5/main.py
+++ Lab5/main.py
@@ -1,4 +1,7 @@
+import torch
 import torch.nn as nn
+import os
+from PIL import Image
 
 
 class Cnn(nn.Module):
@@ -34,3 +37,35 @@ class Cnn(nn.Module):
         self.dropout = nn.Dropout(0.5)
         self.fc2 = nn.Linear(10, 2)
         self.relu = nn.ReLU()
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = out.view(out.size(0), -1)
+        out = self.relu(self.fc1(out))
+        out = self.fc2(out)
+        return out
+
+
+class Dataset(torch.utils.data.Dataset):
+    """
+        Этот класс предназначен для загрузки изображений
+    """
+    def __init__(self, file_list, transform=None):
+        self.transform = transform
+        self.file_list = file_list
+
+    def __len__(self):
+        self.filelength = len(self.file_list)
+        return self.filelength
+
+    def __getitem__(self, idx: int):
+        img = Image.open(self.file_list[idx])
+        img_transformed = self.transform(img.convert("RGB"))
+        label = self.file_list[idx].split('/')[-1].split('.')[0]
+        if label == os.path.join("new_dataset", "rose"):
+            label = 1
+        elif label == os.path.join("new_dataset", "tulip"):
+            label = 0
+        return img_transformed, label
